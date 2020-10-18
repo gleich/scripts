8a4b7bccbc540af0f15711206dbba671e820aafe
Matthew Gleich <email@mattglei.ch>
2020-10-18 14:38:53 -0400

    ♻️ Refactor for new file structure
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- mindnode.py
+++ mindnode.py
@@ -4,9 +4,9 @@ import calendar
 import shutil
 from datetime import datetime
 
-daily_directory_path = '/Users/mattgleich/Library/Mobile Documents/W6L39UYL6Z~com~mindnode~MindNode/Documents/personal/daily'
+daily_directory_path = "/Users/mattgleich/Library/Mobile Documents/W6L39UYL6Z~com~mindnode~MindNode/Documents/tasks"
 # Mindnode files are folders (in package form)
-template_folder_path = daily_directory_path + '/template.mindnode'
+template_folder_path = daily_directory_path + "/tasks.mindnode"
 
 # Creating month folder
 today = datetime.now()
@@ -14,16 +14,17 @@ month_name = (calendar.month_name[today.month]).lower()
 os.chdir(daily_directory_path)
 if not os.path.exists(month_name) and not os.path.isdir(month_name):
     os.mkdir(month_name)
-    print(f'Created the folder {month_name}')
+    print(f"Created the folder {month_name}")
 os.chdir(month_name)
 
 # Creating file from template
-new_folder_path = os.getcwd() + '/{}-{}-{}.mindnode'.format(
-    month_name.title(), today.day, today.year)
+new_folder_path = os.getcwd() + "/{}-{}-{}.mindnode".format(
+    month_name.title(), today.day, today.year
+)
 if os.path.exists(new_folder_path):
-    print('File already exists!')
+    print("File already exists!")
     exit(1)
 shutil.copytree(template_folder_path, new_folder_path)
-print('Created the file')
+print("Created the file")
 
-os.system('open ' + new_folder_path.replace(' ', r'\ '))
+os.system("open " + new_folder_path.replace(" ", r"\ "))
