7af6e79506d6eb5e5c44065d91aacf4505894c0a
Matthew Gleich <email@mattglei.ch>
2020-10-23 11:58:11 -0400

    ✨ Add fgh clean and update

--- update.sh
+++ update.sh
@@ -15,6 +15,8 @@ Command brew\ upgrade\ --cask
 Command brew\ cleanup
 Command mas\ upgrade
 Command npm\ upgrade\ -g
+Command fgh\ update
+Command fgh\ clean
 Command flutter\ upgrade
 cd /Users/mattgleich/github/Matt-Gleich/public/Shell/dots/bin
 Command sh\ push-latest.sh
