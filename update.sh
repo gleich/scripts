e7f408a18fce77044b0468c7a78701e737ca4ef4
Matthew Gleich <email@mattglei.ch>
2020-11-13 23:58:30 -0500

    ✨ Update oh my zsh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 # This program is intended to update homebrew, npm, mas, flutter, and
-# upload the configs for my dot files repo
+# upload the configs for my dot files repo. I run this manually every
+# morning and every night.
 
 Command() {
     COLUMNS=$(tput cols)
@@ -17,5 +18,7 @@ Command mas\ upgrade
 Command npm\ upgrade\ -g
 Command fgh\ update
 Command fgh\ clean
+Command omz\ update
 cd /Users/mattgleich/github/Matt-Gleich/public/Shell/dots/bin
 Command sh\ push-latest.sh
+
