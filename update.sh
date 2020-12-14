974c646f5d638a71d0d224ab13fd1765e0dc0952
Matthew Gleich <email@mattglei.ch>
2020-12-14 00:14:14 -0500

    ✨ Update gitmoji in update script

--- update.sh
+++ update.sh
@@ -19,5 +19,6 @@ Command fgh\ update
 Command fgh\ clean
 Command fgh\ pull
 Command rustup\ update
+Command gitmoji\ --update
 cd /Users/matt/Desktop/Matt-Gleich/public/shell/dots/bin/fetch
 Command poetry\ run\ python3\ main.py
