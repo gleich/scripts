df1d6850e892fd4b24c6c6747c334ff71768d876
Matt Gleich <git@mattglei.ch>
2021-11-06 14:49:44 -0400

    feat: kiwi check and pnpm global update
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- update.sh
+++ update.sh
@@ -13,6 +13,9 @@ Command() {
 Command "brew update"
 Command "brew upgrade"
 Command "brew cleanup -s"
+Command "pnpm update --global"
+cd $(fgh ls senior)
+Command "kiwi check"
 Command "fgh update"
 Command "fgh pull"
 Command "rustup update"
