bbefc51658383321310bc669473c80b0a933b874
Matt Gleich <git@mattglei.ch>
2022-01-25 21:40:22 -0500

    feat: bring back kiwi build script
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ kiwi-build.fish
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This script will build a kiwi branch and used in one of my fish functions.
+
+echo
+echo "--- NEW BUILD INCOMING ---"
+echo 
+
+kiwi build
+
+echo
+echo "--- BUILD DONE ---"
+echo 
