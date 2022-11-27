010405ceb44c5ab5ba1944d8a77bc4841464f483
Matt Gleich <git@mattglei.ch>
2022-11-27 18:00:34 -0500

    feat: port update program to rust
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ update/src/main.rs
@@ -0,0 +1,41 @@
+use std::{path::Path, process};
+
+use anyhow::{Context, Result};
+use task_log::{task, ConfigBuilder};
+
+fn main() -> Result<()> {
+    ConfigBuilder::new()
+        .replace(false)
+        .apply()
+        .expect("Failed to apply custom logging configuration");
+
+    command("brew", vec!["update"], None)?;
+    command("brew", vec!["upgrade"], None)?;
+    command("brew", vec!["cleanup", "-s"], None)?;
+    command("rustup", vec!["update"], None)?;
+    command("cargo", vec!["install-update", "-a"], None)?;
+
+    let rmapi_dir = Path::new("/Users/matt/tmp/rmapi");
+    command("git", vec!["pull", "."], Some(rmapi_dir))?;
+    command("go", vec!["install", "."], Some(rmapi_dir))?;
+
+    command("fetch", vec![], Some(Path::new("/Users/matt/src/dots")))?;
+
+    Ok(())
+}
+
+fn command(binary: &str, args: Vec<&str>, path: Option<&Path>) -> Result<()> {
+    let cmd = format!("{} {}", binary, args.join(" "));
+    task(&cmd, || -> Result<()> {
+        let mut process = process::Command::new(&binary)
+            .args(&args)
+            .current_dir(path.unwrap_or(Path::new("/Users/matt")))
+            .spawn()
+            .context(format!("Failed to run command: {}", cmd))?;
+        process
+            .wait()
+            .context(format!("Failed to wait for {} to finish", cmd))?;
+        Ok(())
+    })?;
+    Ok(())
+}
