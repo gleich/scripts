fd7ed7b529dda476f5060b42b2402f662604c282
Matt Gleich <git@mattglei.ch>
2025-10-13 17:28:55 -0400

    feat: change writer perms for time file

--- update/check.go
+++ update/check.go
@@ -48,7 +48,7 @@ func writeTime(now time.Time, path string) {
 	if err != nil {
 		timber.Fatal(err, "failed to make directory:", dir)
 	}
-	err = os.WriteFile(path, []byte(now.Format(timeFormat)), 0655)
+	err = os.WriteFile(path, []byte(now.Format(timeFormat)), 0500)
 	if err != nil {
 		timber.Fatal(err, "failed to write to file")
 	}
