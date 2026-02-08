b31434236775bfc2cf86c4637c2ec72842027de6
Matt Gleich <git@mattglei.ch>
2026-02-08 11:04:59 -0500

    feat: change time format

--- util/time.go
+++ util/time.go
@@ -7,6 +7,14 @@ import (
 )
 
 func FormatDuration(d time.Duration) string {
+	if d < time.Second {
+		ms := d.Milliseconds()
+		if ms == 0 && d > 0 {
+			ms = 1 // optional: avoid showing "0ms" for tiny >0 durations
+		}
+		return fmt.Sprintf("%dms", ms)
+	}
+
 	var parts []string
 
 	if hours := d / time.Hour; hours > 0 {
@@ -17,13 +25,9 @@ func FormatDuration(d time.Duration) string {
 		parts = append(parts, fmt.Sprintf("%dmin", minutes))
 		d %= time.Minute
 	}
-	if seconds := d / time.Second; seconds > 0 {
-		parts = append(parts, fmt.Sprintf("%ds", seconds))
-		d %= time.Second
-	}
-	if ms := d.Milliseconds(); ms > 0 {
-		parts = append(parts, fmt.Sprintf("%dms", ms))
-	}
+
+	sec := float64(d) / float64(time.Second)
+	parts = append(parts, fmt.Sprintf("%.1fs", sec))
 
 	return strings.Join(parts, " ")
 }
