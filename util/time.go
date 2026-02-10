9b246acbf12e777fcba8a7894348a7e496334a52
Matt Gleich <git@mattglei.ch>
2026-02-10 17:57:38 -0500

    feat: remove comment

--- util/time.go
+++ util/time.go
@@ -10,7 +10,7 @@ func FormatDuration(d time.Duration) string {
 	if d < time.Second {
 		ms := d.Milliseconds()
 		if ms == 0 && d > 0 {
-			ms = 1 // optional: avoid showing "0ms" for tiny >0 durations
+			ms = 1
 		}
 		return fmt.Sprintf("%dms", ms)
 	}
