860d2b457d2f38f003303e3f177211405beaff63
Matt Gleich <git@mattglei.ch>
2026-03-26 15:43:46 -0400

    refactor: parameterize timerand with count and total time input

--- timerand/timerand.go
+++ timerand/timerand.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"fmt"
 	"math/big"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 
 	"go.mattglei.ch/scripts/internal/logger"
@@ -15,18 +19,50 @@ import (
 func main() {
 	logger.Setup()
 
-	for {
-		const min = 2 * time.Minute
-		const span = 3 * time.Minute
-		n, err := rand.Int(rand.Reader, big.NewInt(int64(span)))
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Count: ")
+	scanner.Scan()
+	count, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	if err != nil || count <= 0 {
+		timber.Fatal(err, "invalid count")
+	}
+
+	fmt.Print("Total time (minutes): ")
+	scanner.Scan()
+	totalMinutes, err := strconv.ParseFloat(strings.TrimSpace(scanner.Text()), 64)
+	if err != nil || totalMinutes <= 0 {
+		timber.Fatal(err, "invalid total time")
+	}
+	totalTime := time.Duration(totalMinutes * float64(time.Minute))
+
+	// Generate random weights and normalize to sum to totalTime
+	weights := make([]int64, count)
+	var weightSum int64
+	for i := range count {
+		n, err := rand.Int(rand.Reader, big.NewInt(1_000_000))
 		if err != nil {
 			timber.Fatal(err, "failed to get random number")
 		}
-		waitTime := min + time.Duration(n.Int64())
-		waitTimeFormatted := util.FormatDuration(waitTime)
+		// avoid zero weights
+		weights[i] = n.Int64() + 1
+		weightSum += weights[i]
+	}
 
+	intervals := make([]time.Duration, count)
+	var allocated time.Duration
+	for i := range count {
+		if i == count-1 {
+			intervals[i] = totalTime - allocated
+		} else {
+			intervals[i] = time.Duration(int64(totalTime) * weights[i] / weightSum)
+			allocated += intervals[i]
+		}
+	}
+
+	for i, waitTime := range intervals {
 		start := time.Now()
-		timber.Info("Waiting for", waitTimeFormatted)
+		timber.Info(fmt.Sprintf("[%d/%d]", i+1, count), "Waiting for", util.FormatDuration(waitTime))
 
 		time.Sleep(waitTime)
 		timber.DoneSince(start, "sending notification")
@@ -35,7 +71,7 @@ func main() {
 			"osascript", "-e",
 			fmt.Sprintf(
 				`display notification %q with title %q`,
-				fmt.Sprintf("%s is up", waitTimeFormatted),
+				fmt.Sprintf("%d of %d", i+1, count),
 				"timerand",
 			)).Run()
 		if err != nil {
@@ -49,4 +85,6 @@ func main() {
 			}
 		}
 	}
+
+	timber.Done("all done")
 }
