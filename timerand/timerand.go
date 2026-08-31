b013f33c43d2b742407283243957813f147cdcf2
Matt Gleich <git@mattglei.ch>
2026-08-30 20:00:00 -0400

    feat(timerand): restore random mode for zero count

--- timerand/timerand.go
+++ timerand/timerand.go
@@ -24,15 +24,24 @@ func main() {
 	fmt.Print("Count: ")
 	scanner.Scan()
 	count, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
-	if err != nil || count <= 0 {
+	if err != nil {
 		timber.Fatal(err, "invalid count")
+	}
+	if count < 0 {
+		timber.Fatal(fmt.Errorf("count must be at least 0"), "invalid count")
+	}
+	if count == 0 {
+		runRandomTimer()
 	}
 
 	fmt.Print("Total time (minutes): ")
 	scanner.Scan()
 	totalMinutes, err := strconv.ParseFloat(strings.TrimSpace(scanner.Text()), 64)
-	if err != nil || totalMinutes <= 0 {
+	if err != nil {
 		timber.Fatal(err, "invalid total time")
+	}
+	if totalMinutes <= 0 {
+		timber.Fatal(fmt.Errorf("total time must be greater than 0"), "invalid total time")
 	}
 	totalTime := time.Duration(totalMinutes * float64(time.Minute))
 
@@ -61,30 +70,56 @@ func main() {
 	}
 
 	for i, waitTime := range intervals {
-		start := time.Now()
-		timber.Infof("[%d/%d] waiting for %s", i+1, count, util.FormatDuration(waitTime))
-
-		time.Sleep(waitTime)
-		timber.DoneSince(start, "sending notification")
-
-		err = exec.Command(
-			"osascript", "-e",
-			fmt.Sprintf(
-				`display notification %q with title %q`,
-				fmt.Sprintf("%d of %d", i+1, count),
-				"timerand",
-			)).Run()
-		if err != nil {
-			timber.Fatal(err, "failed to display notification")
-		}
-
-		for range 2 {
-			err = exec.Command("afplay", "/System/Library/Sounds/Ping.aiff").Run()
-			if err != nil {
-				timber.Fatal(err, "failed to play ping sound")
-			}
-		}
+		progress := fmt.Sprintf("[%d/%d]", i+1, count)
+		notification := fmt.Sprintf("%d of %d", i+1, count)
+		waitAndNotify(waitTime, progress, notification)
 	}
 
 	timber.Done("all done")
+}
+
+func runRandomTimer() {
+	const minWait = 2 * time.Minute
+	const waitRange = 3 * time.Minute
+
+	for {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(waitRange)))
+		if err != nil {
+			timber.Fatal(err, "failed to get random number")
+		}
+		waitTime := minWait + time.Duration(n.Int64())
+		waitTimeFormatted := util.FormatDuration(waitTime)
+		waitAndNotify(waitTime, "", fmt.Sprintf("%s is up", waitTimeFormatted))
+	}
+}
+
+func waitAndNotify(waitTime time.Duration, progress string, notification string) {
+	start := time.Now()
+	waitTimeFormatted := util.FormatDuration(waitTime)
+	if progress == "" {
+		timber.Infof("waiting for %s", waitTimeFormatted)
+	} else {
+		timber.Infof("%s waiting for %s", progress, waitTimeFormatted)
+	}
+
+	time.Sleep(waitTime)
+	timber.DoneSince(start, "sending notification")
+
+	err := exec.Command(
+		"osascript", "-e",
+		fmt.Sprintf(
+			`display notification %q with title %q`,
+			notification,
+			"timerand",
+		)).Run()
+	if err != nil {
+		timber.Fatal(err, "failed to display notification")
+	}
+
+	for range 2 {
+		err = exec.Command("afplay", "/System/Library/Sounds/Ping.aiff").Run()
+		if err != nil {
+			timber.Fatal(err, "failed to play ping sound")
+		}
+	}
 }
